029f8c5ffd5ce94b3db695033a02752d9e833031
SosukeKato <dokindokinpiano@gmail.com>
2025-10-16 22:04:02 +0900

    [update]ジャンプの処理を追加

--- Assets/_WorkSpace/Script/PlayerMove.cs
+++ Assets/_WorkSpace/Script/PlayerMove.cs
@@ -11,6 +11,8 @@ public class PlayerMove : MonoBehaviour
 
     [SerializeField]
     float _moveSpeed;
+    [SerializeField]
+    float _jumpPower;
     void Start()
     {
         _tr = transform;
@@ -19,7 +21,21 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
+        #region ˆÚ“®
         _move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         _tr.position += new Vector3(_move.x * Time.deltaTime * _moveSpeed, 0, _move.z * Time.deltaTime * _moveSpeed);
+        #endregion
+
+        #region ƒWƒƒƒ“ƒv
+        //Ú’n”»’è‚Ìˆ—‚ğ’Ç‰Á—\’è
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(0, _jumpPower, 0);
+        }
+        #endregion
+
+        #region UŒ‚
+
+        #endregion
     }
 }
