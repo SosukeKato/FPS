1329cfe7352405182da5501d5f96c14f541de0db
SosukeKato <dokindokinpiano@gmail.com>
2025-10-16 21:57:18 +0900

    [update]Playerの前後左右の移動を実装

--- Assets/_WorkSpace/Script/PlayerMove.cs
+++ Assets/_WorkSpace/Script/PlayerMove.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Transform _tr;
+    Rigidbody _rb;
+
+    Vector3 _move;
+    Vector3 _origin;
+    Vector3 _front;
+
+    [SerializeField]
+    float _moveSpeed;
     void Start()
     {
-        
+        _tr = transform;
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        _tr.position += new Vector3(_move.x * Time.deltaTime * _moveSpeed, 0, _move.z * Time.deltaTime * _moveSpeed);
     }
 }
